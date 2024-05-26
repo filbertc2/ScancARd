19f532bc4101f397d4059bb875d4bdfe66f671e2
Filbert <filbertc2@gmail.com>
2024-05-27 00:30:02 +0700

    Final Version

---
+++ ScancARd/Data/NodeActions.swift
@@ -0,0 +1,25 @@
+import Foundation
+
+struct NodeActions {
+    static let phoneNumbers: [String: String] = [
+        "call1": "+6281234567891",
+        "call2": "+6281234567892",
+        "call3": "+6281234567893",
+        "call4": "+6281234567894"
+    ]
+    
+    static let emailRecipients: [String: String] = [
+        "email1": "steven@icloud.com",
+        "email2": "vincent@icloud.com",
+        "email3": "jason@icloud.com",
+        "email4": "james@icloud.com"
+    ]
+    
+    static let mapCoordinates: [String: String] = [
+        "map1": "6.30168S,106.65109E",
+        "map2": "6.30169S,106.65110E",
+        "map3": "6.30170S,106.65111E",
+        "map4": "6.30171S,106.65112E"
+    ]
+}
+
--- ScancARd/Main/ViewController.swift
+++ ScancARd/Main/ViewController.swift
@@ -1,11 +1,3 @@
-//
-//  ViewController.swift
-//  Nano Challenge 2
-//
-//  Created by Filbert Chai on 15/05/24.
-//
-
-
 import UIKit
 import SceneKit
 import ARKit
@@ -19,10 +11,7 @@ class ViewController: UIViewController, ARSCNViewDelegate, UIGestureRecognizerDe
         super.viewDidLoad()
         
         sceneView.delegate = self
-        
-        let scene = SCNScene()
-        
-        sceneView.scene = scene
+        sceneView.scene = SCNScene()
         
         let tapGesture = UITapGestureRecognizer(target: self, action: #selector(handleTap(_:)))
         sceneView.addGestureRecognizer(tapGesture)
@@ -33,7 +22,7 @@ class ViewController: UIViewController, ARSCNViewDelegate, UIGestureRecognizerDe
         super.viewWillAppear(animated)
         
         let configuration = ARImageTrackingConfiguration()
-        guard let referenceImages = ARReferenceImage.referenceImages(inGroupNamed: "AR Resources", bundle: nil) else{return }
+        guard let referenceImages = ARReferenceImage.referenceImages(inGroupNamed: "AR Resources", bundle: nil) else { return }
         configuration.trackingImages = referenceImages
         
         sceneView.session.run(configuration)
@@ -41,7 +30,6 @@ class ViewController: UIViewController, ARSCNViewDelegate, UIGestureRecognizerDe
     
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
-        
         sceneView.session.pause()
     }
     
@@ -49,17 +37,9 @@ class ViewController: UIViewController, ARSCNViewDelegate, UIGestureRecognizerDe
         return true
     }
     
-    func session(_ session: ARSession, didFailWithError error: Error) {
-        
-    }
-    
-    func sessionWasInterrupted(_ session: ARSession) {
-        
-    }
-    
-    func sessionInterruptionEnded(_ session: ARSession) {
-        
-    }
+    func session(_ session: ARSession, didFailWithError error: Error) { }
+    func sessionWasInterrupted(_ session: ARSession) { }
+    func sessionInterruptionEnded(_ session: ARSession) { }
 }
 
 extension ViewController {
@@ -77,106 +57,49 @@ extension ViewController {
     }
     
     private func addPlaneForAnchor(_ anchor: ARImageAnchor, node: SCNNode) {
-            switch anchor.referenceImage.name {
-            case "card1":
-                ARContentModel1.addPlane(anchor: anchor, node: node)
-            case "card2":
-                ARContentModel2.addPlane(anchor: anchor, node: node)
-            case "card3":
-                ARContentModel3.addPlane(anchor: anchor, node: node)
-            case "card4":
-                ARContentModel4.addPlane(anchor: anchor, node: node)
-            default:
-                break
-            }
+        switch anchor.referenceImage.name {
+        case "card1":
+            ARContentModel1.addPlane(anchor: anchor, node: node)
+        case "card2":
+            ARContentModel2.addPlane(anchor: anchor, node: node)
+        case "card3":
+            ARContentModel3.addPlane(anchor: anchor, node: node)
+        case "card4":
+            ARContentModel4.addPlane(anchor: anchor, node: node)
+        default:
+            break
         }
+    }
     
     @objc func handleTap(_ gestureRecognizer: UITapGestureRecognizer) {
         let location = gestureRecognizer.location(in: sceneView)
         let hitTestResults = sceneView.hitTest(location, options: nil)
-        if let hitNode = hitTestResults.first?.node {
-            switch hitNode.name {
-            case "call1":
-                if let url = URL(string: "tel://+6281234567891") {
-                    UIApplication.shared.open(url, options: [:], completionHandler: nil)
-                }
-            case "email1":
-                sendCustomEmail1()
-            case "map1":
-                if let url = URL(string: "http://maps.apple.com/?q=6,30168S,106,65109E") {
-                    UIApplication.shared.open(url, options: [:], completionHandler: nil)
-                }
-            case "call2":
-                if let url = URL(string: "tel://+6281234567892") {
-                    UIApplication.shared.open(url, options: [:], completionHandler: nil)
-                }
-            case "email2":
-                sendCustomEmail2()
-            case "map2":
-                if let url = URL(string: "http://maps.apple.com/?q=6,30168S,106,65109E") {
-                    UIApplication.shared.open(url, options: [:], completionHandler: nil)
-                }
-            case "call3":
-                if let url = URL(string: "tel://+6281234567893") {
-                    UIApplication.shared.open(url, options: [:], completionHandler: nil)
-                }
-            case "email3":
-                sendCustomEmail3()
-            case "map3":
-                if let url = URL(string: "http://maps.apple.com/?q=6,30168S,106,65109E") {
-                    UIApplication.shared.open(url, options: [:], completionHandler: nil)
-                }
-            case "call4":
-                if let url = URL(string: "tel://+6281234567894") {
-                    UIApplication.shared.open(url, options: [:], completionHandler: nil)
-                }
-            case "email4":
-                sendCustomEmail4()
-            case "map4":
-                if let url = URL(string: "http://maps.apple.com/?q=6,30168S,106,65109E") {
-                    UIApplication.shared.open(url, options: [:], completionHandler: nil)
-                }
-            default:
-                break
-            }
+        if let hitNode = hitTestResults.first?.node, let nodeName = hitNode.name {
+            print(nodeName)
+            handleNodeTap(nodeName: nodeName)
         }
     }
     
-    func sendCustomEmail1() {
-        guard MFMailComposeViewController.canSendMail() else {
-            let alertController = UIAlertController(title: "Error", message: "Your device cannot send email.", preferredStyle: .alert)
-            alertController.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
-            self.present(alertController, animated: true, completion: nil)
-            return
+    
+    private func handleNodeTap(nodeName: String) {
+        if let phoneNumber = NodeActions.phoneNumbers[nodeName] {
+            openURL(urlString: "tel://\(phoneNumber)")
+        } else if let emailRecipient = NodeActions.emailRecipients[nodeName] {
+            sendEmail(to: emailRecipient)
+        } else if let coordinates = NodeActions.mapCoordinates[nodeName] {
+            openURL(urlString: "http://maps.apple.com/?q=\(coordinates)")
         }
-        
-        let mailComposer = MFMailComposeViewController()
-        mailComposer.mailComposeDelegate = self
-        mailComposer.setToRecipients(["steven@icloud.com"])
-        mailComposer.setSubject("Subject")
-        mailComposer.setMessageBody("Message body", isHTML: false)
-        
-        self.present(mailComposer, animated: true, completion: nil)
     }
     
-    func sendCustomEmail2() {
-        guard MFMailComposeViewController.canSendMail() else {
-            let alertController = UIAlertController(title: "Error", message: "Your device cannot send email.", preferredStyle: .alert)
-            alertController.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
-            self.present(alertController, animated: true, completion: nil)
-            return
+    
+    
+    private func openURL(urlString: String) {
+        if let url = URL(string: urlString) {
+            UIApplication.shared.open(url, options: [:], completionHandler: nil)
         }
-        
-        let mailComposer = MFMailComposeViewController()
-        mailComposer.mailComposeDelegate = self
-        mailComposer.setToRecipients(["vincent@icloud.com"])
-        mailComposer.setSubject("Subject")
-        mailComposer.setMessageBody("Message body", isHTML: false)
-        
-        self.present(mailComposer, animated: true, completion: nil)
     }
     
-    func sendCustomEmail3() {
+    private func sendEmail(to recipient: String) {
         guard MFMailComposeViewController.canSendMail() else {
             let alertController = UIAlertController(title: "Error", message: "Your device cannot send email.", preferredStyle: .alert)
             alertController.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
@@ -186,23 +109,7 @@ extension ViewController {
         
         let mailComposer = MFMailComposeViewController()
         mailComposer.mailComposeDelegate = self
-        mailComposer.setToRecipients(["jason@icloud.com"])
-        mailComposer.setSubject("Subject")
-        mailComposer.setMessageBody("Message body", isHTML: false)
-        
-        self.present(mailComposer, animated: true, completion: nil)
-    }
-    func sendCustomEmail4() {
-        guard MFMailComposeViewController.canSendMail() else {
-            let alertController = UIAlertController(title: "Error", message: "Your device cannot send email.", preferredStyle: .alert)
-            alertController.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
-            self.present(alertController, animated: true, completion: nil)
-            return
-        }
-        
-        let mailComposer = MFMailComposeViewController()
-        mailComposer.mailComposeDelegate = self
-        mailComposer.setToRecipients(["james@icloud.com"])
+        mailComposer.setToRecipients([recipient])
         mailComposer.setSubject("Subject")
         mailComposer.setMessageBody("Message body", isHTML: false)
         
